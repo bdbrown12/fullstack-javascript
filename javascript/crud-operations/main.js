a95a17a5f55ed705b6e2a3727ade7dc2764df67a
bdbrown12 <123220632+bdbrown12@users.noreply.github.com>
2025-02-05 21:36:41 -0700

    Added CRUD operations to the project

--- javascript/crud-operations/main.js
+++ javascript/crud-operations/main.js
@@ -11,13 +11,46 @@ document.addEventListener("DOMContentLoaded", function() {
         formValidation();
     });
 
+    //prevent users from submitting a blank field
     let formValidation = () => {
         if (input.value === "") {
             msg.innerHTML = "Post cannot be blank";
             console.log("failure");
         } else {
-            console.log("successs");
+            console.log("success");
             msg.innerHTML = "";
+            acceptData();
         }
+    }
+
+    let data = {};
+
+    let acceptData = () => {
+        data["text"] = input.value;
+        console.log(data);
+
+        createPost();
+    };
+
+    let createPost = () => {
+        posts.innerHTML += `
+                <div>
+                    <p>${data.text}</p>
+                    <span class="options">
+                        <i onclick="editPost(this)" class="fas fa-edit"></i>
+                        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+                    </span>
+                </div>
+        `;
+        input.value = "";
     };
 });
+
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+};
+
+let editPost = (e) => {
+    input.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+}
