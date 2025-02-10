5138ce8b72fb13dd1bb6a41535d5cdcf00ba0646
brownka98 <10962606@uvu.edu>
2025-02-10 14:44:09 -0700

    Created an outline of many of the tasks that my tic tac toe game will need to do. Began writing some of the code in my runGame function that will execute upon game start

--- javascript/tic-tac-toe-project/main.js
+++ javascript/tic-tac-toe-project/main.js
@@ -3,26 +3,78 @@
 //create a Gameboard object
 
 function GameBoard() {
-    let gameArray = ['', '', ''];
+    let gameArray = ['', '', '',
+                     '', '', '',
+                     '', '', ''];
 
     const player = Player("Kaleb", "X");
+    const opponent = Player("Computer", "O");
 
-    runGame(gameArray, player);
+    runGame(gameArray, player, opponent);
 }
 
 function Player(name, marker, score) {
     this.name = name;
     this.marker = marker;
     this.score = score;
+    this.isTurn = isTurn = false;
 
-    return {name, marker, score};
+    return {name, marker, score, isTurn};
 }
 
-function runGame(array, player) {
-    //what do i want to happen when i run the game?
+function runGame(array, player, opponent) {
+    //what happens when you start a game of tik tac toe?
 
-    //let the player place their marker anywhere on the board
-    let playerMarker = player.marker;
+    //figure out who's turn it is
+    console.log(player.isTurn);
+    console.log(opponent.isTurn);
+
+    //check if the board is filled or not
+    let boardFull = array.every((index) => {
+        index !== '';
+    })
+
+    if (boardFull){
+        console.log('Game Over')
+    } else {
+        console.log('Keep playing');
+        //playerTurn();
+        //opponentTurn();
+    }
+
+    // if (boardFull) board is full
+    
+        //end game
+
+    // else
+        // if (isturn)
+            // playerTurn()
+        // else
+            // oppentTurn()
+
+
+
+    // end game
+
+    //you select either X or O (make this dynamic later)
+    const playerMarker = player.marker; // X
+    const opponentMarker = opponent.marker; // O
+
+    function playerTurn() {
+        //you then place your marker on the board
+        array[0] = playerMarker;
+        console.log(array);
+
+        //change turn status
+    }
+
+
+    //your turn ends
+
+    //the computer/opponent can now place their marker on the board
+    function opponentTurn() {
+        
+    }
 
     //the computer should then place their marker
 
