1f27d9a0269b825d26fc51c4c2ecfdd49d7cbb98
brownka98 <10962606@uvu.edu>
2025-02-11 14:21:33 -0700

    Created functions for player turns that allow the player and opponent to add their markers to the game array

--- javascript/tic-tac-toe-project/main.js
+++ javascript/tic-tac-toe-project/main.js
@@ -3,14 +3,18 @@
 //create a Gameboard object
 
 function GameBoard() {
-    let gameArray = ['', '', '',
+    let gameArray = ['X', '', '',
                      '', '', '',
                      '', '', ''];
 
     const player = Player("Kaleb", "X");
     const opponent = Player("Computer", "O");
 
+    player.isTurn = true; //Change later, makes it so the player always goes first
+
     runGame(gameArray, player, opponent);
+
+    return {gameArray, player, opponent}
 }
 
 function Player(name, marker, score) {
@@ -25,45 +29,63 @@ function Player(name, marker, score) {
 function runGame(array, player, opponent) {
     //what happens when you start a game of tik tac toe?
 
+    //you select either X or O (make this dynamic later)
+    const playerMarker = player.marker; // X
+    const opponentMarker = opponent.marker; // O
+
     //figure out who's turn it is
     console.log(player.isTurn);
     console.log(opponent.isTurn);
 
     //check if the board is filled or not
     let boardFull = array.every((index) => {
-        index !== '';
-    })
+        return index !== '';
+    });
 
     if (boardFull){
         console.log('Game Over')
     } else {
         console.log('Keep playing');
-        //playerTurn();
-        //opponentTurn();
+
+        if (player.isTurn) {
+            playerTurn(player, array);
+        } else {
+            opponentTurn();
+        }
     }
 
-    // if (boardFull) board is full
-    
-        //end game
-
-    // else
-        // if (isturn)
-            // playerTurn()
-        // else
-            // oppentTurn()
 
 
 
     // end game
 
-    //you select either X or O (make this dynamic later)
-    const playerMarker = player.marker; // X
-    const opponentMarker = opponent.marker; // O
+    function playerTurn(player, array) {
+        //if grid spot has value
 
-    function playerTurn() {
-        //you then place your marker on the board
-        array[0] = playerMarker;
-        console.log(array);
+        //how do i get the value of each item in an array and update it in the array?
+        array.forEach((index, array) => {
+            let playerSelection = 2; //based on what the player selects
+
+            //grab that array element and set it to the player's marker
+            if (index === playerSelection) {
+                array[index] = playerMarker;
+            }
+            
+            //render the changes to the display
+            //updateArray(array)
+
+            //change turns
+            player.isTurn = false;
+            opponent.isTurn = true;
+        });
+
+        //what do i need in a function that updates the array?
+        //first, it should be passed the array
+        
+
+        //player places marker on the board
+
+        //player can't place if spot has value
 
         //change turn status
     }
@@ -81,4 +103,5 @@ function runGame(array, player, opponent) {
     //if the player or the computer get 3 in a row increment their score by 1
 }
 
-GameBoard();
+let game = GameBoard();
+console.log(game.gameArray);
