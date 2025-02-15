f1c84ec18946085a5a539511e0a7ddc8ca8d41e2
bdbrown12 <123220632+bdbrown12@users.noreply.github.com>
2025-02-14 17:06:52 -0700

    Did the js.info lesson on property accessors.

--- javascript/classes/getters-and-setters/main.js
+++ javascript/classes/getters-and-setters/main.js
@@ -6,14 +6,46 @@ let user = {
         return `${this.name} ${this.surname}`;
     },
 
-    set fullName(value){
+    set fullName(value) {
         [this.name, this.surname] = value.split(" ");
     }
 };
 
-alert(user.fullName) //we don't call the accessor property WE READ IT
+// alert(user.fullName) //we don't call the accessor property WE READ IT
 
-user.fullName = "Alice Cooper";
+// user.fullName = "Alice Cooper";
 
-alert(user.name) //Alice
-alert(user.surname) //Cooper
+// alert(user.name) //Alice
+// alert(user.surname) //Cooper
+
+let user1 = {
+    get name() {
+        return this._name;
+    },
+
+    set name(value) {
+        if (value.length < 4) {
+            alert("Name is too short, need at least 4 characters.");
+            return;
+        }
+        this._name = value;
+    }
+}
+
+//using for compatibility
+
+function User(name, birthday) {
+    this.name = name;
+    this.birthday = birthday;
+
+    Object.defineProperty(this, "age", {
+        get() {
+            let todayYear = new Date().getFullYear();
+            return todayYear - this.birthday.getFullYear();
+        }
+    });
+
+    return { name, birthday };
+}
+
+let john = User("John", new Date(1992, 6, 1));
