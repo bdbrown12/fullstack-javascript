867a4140468f5173f8a11063ef74c15139a55b44
bdbrown12 <123220632+bdbrown12@users.noreply.github.com>
2025-02-21 22:43:12 -0700

    Added mergesort function

---
+++ javascript/comp-sci/recursion/project/main.js
@@ -0,0 +1,47 @@
+function fib(n) {
+    const fib = [0, 1]
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+}
+
+
+function fibsRec(n) {
+    const fib = [0, 1]
+
+    //base case
+    if (n < 2) {
+        return n;
+    }
+
+    return fibsRec(n-1) + fibsRec(n-2);
+
+
+
+    //returns an array containing that many numbers from the fibonacci sequence
+}
+
+//takes an array
+//returns a sorted array
+let arr = [8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+
+function mergeSort(arr) {
+    //base case
+    if (arr.length <= 1) return arr;
+    //else
+    // divide array in half
+    const arrLength = arr.length
+    const mid = Math.floor(arrLength / 2)
+
+    // how do i get the left half?
+    const left = arr.slice(0, mid)
+
+    // how do i get the right half?
+    const right = arr.slice(mid)
+
+            // if leftarr[]
+
+            //return mergeSort()
+}
