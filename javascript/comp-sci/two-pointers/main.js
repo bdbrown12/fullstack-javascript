3577a0932c8b0209af09ea7402ddb40bedc24257
bdbrown12 <123220632+bdbrown12@users.noreply.github.com>
2025-03-01 00:58:45 -0700

    Created a twoSum and sortArr function to practice comparing array data against itself.

---
+++ javascript/comp-sci/two-pointers/main.js
@@ -0,0 +1,64 @@
+//find if there exists any pair of elements such that their sum is equal to the target.
+
+// Naive Method – O(n^2) Time and O(1) Space
+
+function twoSum(arr, target) {
+    let n = arr.length;
+
+    for (let i = 0; i < n; i++){
+        for (let j = i + 1; j < n; j++){
+            if (arr[i] + arr[j] === target) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+
+}
+
+let arr = [0, -1, 2, -3, 1];
+let target = -2;
+
+
+// Call the twoSum function and print the result
+if (twoSum(arr, target))
+    console.log("true");
+else 
+    console.log("false");
+
+
+let unsortedArr = [1,4,45,6,10,-8];
+
+function sortArr(arr) {
+    let n = arr.length;
+    //use a selection sort
+
+    //compare the smallest found # to the next element. After loop is complete, swap smallest w/ array[i]
+
+    for (let i = 0; i < n; i ++){
+        let smallestIndex = i;
+
+        //find the smallest element in the remaining array
+        for (let j = i + 1; j < n; j++){
+            if (arr[j] < arr[smallestIndex]) {
+                smallestIndex = j;
+            }
+        }
+
+        if (smallestIndex !== i) {
+            //switch the first element of the array with the smallestindex
+            let temp = arr[i];
+            arr[i] = arr[smallestIndex];
+            arr[smallestIndex] = temp;
+        }
+
+        return arr;
+
+        //compare two array elements
+        
+    }
+
+}
+
+
