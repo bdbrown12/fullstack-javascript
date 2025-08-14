53143b2e2821d358950115955eaa371bfe304630
bdbrown12 <123220632+bdbrown12@users.noreply.github.com>
2025-08-14 08:12:27 -0600

    Added exercises for interview prepartion.

---
+++ javascript/async-and-apis/async-code/main.js
@@ -0,0 +1,44 @@
+// callbacks - a function passed inside of another function which
+            // is invoked inside the outer function to complete some
+            // kind of routine or action
+
+// const myDiv = document.createElement("div");
+// myDiv.addEventListener("click", function(){
+//     //Do something
+// });
+
+// this is a synchronous function, meaning it happens right away, BEFORE THE FUNCTION RETURNS
+
+function callback(val){
+    console.log(val);
+}
+
+
+let fruits = ["banana", "apple", "pear"];
+fruits.forEach(callback);
+console.log("Done");
+
+// ***callbacks are just a way to store things to do at a later time***
+
+// PROMISES
+
+// a promise is an object that might produce a value at some point in the future
+
+// syntax
+
+let p = new Promise((resolve, reject) => {
+    let a = 1 + 1
+
+    if (a === 2){
+        resolve('Success');
+    } else {
+        reject('Failed');
+    }
+})
+
+// interacting with promises
+p.then((message) => {
+    console.log('This is in the then ' + message);
+}).catch((message) => {
+    console.log('This is in the catch ' + message);
+}); //then runs on resolve
